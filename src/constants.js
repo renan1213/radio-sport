dff6020dd1bd7a81774a255e01b4d3cfab37dac4
renan1213 <pedrorens467@gmail.com>
2025-06-14 20:57:22 -0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,9 @@
+// 🔐 Chave de API da NewsAPI (verifique limites e validade)
 export const API_KEY = 'bb64141a0243fe89015579b8f24be5f3';
+
+// 📸 Redes Sociais do Programa
 export const INSTAGRAM_USERNAME = 'pedroregisalenda';
 export const FACEBOOK_USERNAME = 'pedroregisalenda';
+
+// 📞 Número de telefone com DDI +55 (Brasil) e DDD
 export const PHONE_NUMBER = '5588992557103';
