a7cc62b930e30bad3132d97c13302eea9970778a
renan1213 <pedrorens467@gmail.com>
2025-06-14 21:03:06 -0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_KEY = '561b4cdc7bec40918cab071b19ed0590';
+export const API_KEY = 'c296fb39fdab402c80aaab8da61a8638';
 export const INSTAGRAM_USERNAME = 'pedroregisalenda';
 export const FACEBOOK_USERNAME = 'pedroregisalenda';
 export const PHONE_NUMBER = '5588992557103';
